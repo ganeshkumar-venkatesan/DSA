36456f9c8357374693a9fa5fc3da0b5cdbcfcf12
ganeshkumar venatesan <ganeshkumar.v@infosys.com>
2022-11-28 14:06:27 +0530

    fix: encode and decode string

--- src/com/neetcode/dsa/arraysandhashing/EncodeAndDecodeString.java
+++ src/com/neetcode/dsa/arraysandhashing/EncodeAndDecodeString.java
@@ -1,28 +1,26 @@
 package com.neetcode.dsa.arraysandhashing;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class EncodeAndDecodeString {
     public static void main(String[] args) {
         EncodeAndDecodeString encodeAndDecodeString = new EncodeAndDecodeString();
-        String encoded = encodeAndDecodeString.encode(Arrays.asList("lint","code","love","you"));
+        String encoded = encodeAndDecodeString.encode(Arrays.asList("lint", "code", ":", "love", "you"));
         System.out.println(encodeAndDecodeString.decode(encoded));
     }
 
     public String encode(List<String> strs) {
-        String output ="";
+        String output = "";
         for (int j = 0; j < strs.size(); j++) {
             output += strs.get(j);
-            if(j != strs.size()-1){
+            if (j != strs.size() - 1) {
                 output += "&";
             }
         }
         return output;
 
         /* use below code if the input may contain any special character
-        String [] encodingHelper = {"^","@","#","$","%","&",";",":"};
+        String [] encodingHelper = {"@","#","$","%","&",";",":"};
         int i=0;
         boolean findFlag = false;
         do{
@@ -39,7 +37,18 @@ public class EncodeAndDecodeString {
     }
 
     public List<String> decode(String str) {
-        String[] strArray = str.replaceAll("\\W+"," ").split(" ");
+        //if there isn't gonna be any special character in input
+        //String[] strArray = str.replaceAll("\\W+"," ").split(" ");
+
+        String specialCharacterString = str.replaceAll("[a-zA-Z]", "");
+        Map<Character, Integer> specialCharacterMap = new HashMap<>();
+        for (char a : specialCharacterString.toCharArray()) {
+            specialCharacterMap.put(a, specialCharacterMap.getOrDefault(a, 0) + 1);
+        }
+        Queue<Map.Entry<Character, Integer>> keyMapQueue = new PriorityQueue<>((a, b) -> a.getValue() > b.getValue() ? -1 : 1);
+        keyMapQueue.addAll(specialCharacterMap.entrySet());
+        Character s = keyMapQueue.poll().getKey();
+        String[] strArray = str.replaceAll(s.toString(), " ").split(" ");
         return new ArrayList<>(Arrays.asList(strArray));
     }
 }
